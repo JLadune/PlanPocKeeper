50d577caaed4f232ca6e862e98b12992f3690982
Med-Souki <soukimohamedcpge1973@gmail.com>
2026-03-24 23:16:59 -0400

    Added a 2s splash screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,4 +62,7 @@ dependencies {
     implementation("com.google.firebase:firebase-firestore-ktx")
     implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation("androidx.compose.material:material-icons-extended")
+    //SVG usage :]
+    implementation("io.coil-kt:coil-compose:2.6.0")
+    implementation("io.coil-kt:coil-svg:2.6.0")
 }
--- app/src/main/java/com/example/planpockeeper/MainActivity.kt
+++ app/src/main/java/com/example/planpockeeper/MainActivity.kt
@@ -3,7 +3,12 @@ package com.example.planpockeeper
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.planpockeeper.ui.auth.AuthScreen
+import com.example.planpockeeper.ui.splash.SplashScreen
 import com.example.planpockeeper.ui.theme.PlanPocKeeperTheme
 
 class MainActivity : ComponentActivity() {
@@ -11,8 +16,31 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             PlanPocKeeperTheme {
-                AuthScreen()
+                AppNavigation()
             }
+        }
+    }
+}
+
+@Composable
+fun AppNavigation() {
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = "splash"
+    ) {
+        composable("splash") {
+            SplashScreen(
+                onFinished = {
+                    navController.navigate("auth") {
+                        popUpTo("splash") { inclusive = true }
+                    }
+                }
+            )
+        }
+        composable("auth") {
+            AuthScreen()
         }
     }
 }
